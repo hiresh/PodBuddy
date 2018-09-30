d7fa2c73c763106766a36740a678df3c5adb5545
Hiresh Trivedi <hiresh.at@gmail.com>
2018-09-30 21:32:20 +0530

    support for unique user registration

--- saveMySQL/popup.js
+++ saveMySQL/popup.js
@@ -34,7 +34,7 @@ function displayRegisterForm(){
 				 }
 	xhr.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-				 alert(this.responseText);
+				 //alert(this.responseText);
 				 if(this.responseText && this.responseText!='false'){
 					chrome.storage.local.set({"userName":userId},function(){
 								displayUserQueries();
