5a9392644af9667b6d23e6ab9a2fc9ab6a8293c0
Hiresh Trivedi <hiresh.at@gmail.com>
2018-10-01 20:27:00 +0530

    To support Add text via context menu

--- saveMySQL/background.js
+++ saveMySQL/background.js
@@ -1,10 +1,40 @@
+
+var urlPrefix = "http://localhost:8080/podbuddy"
 chrome.contextMenus.create ({
     title: "Add SQL to Repository",
     contexts: ["selection"],
-    onclick: saveToServer
+    onclick: saveQueryToServer
 })
+var userName="";
 
-function saveToServer(text) {
-    alert(text.selectionText);
+chrome.storage.local.get(['userName'],function(result){
+	if(result.userName && result.userName!=''){
+		userName=result.userName;
+	}
+	
+		
+});
+
+function saveQueryToServer(text) {
+	var query={
+				queryText:text.selectionText,
+				queryName:"Query",
+				description:"added from contextMenu",
+				author:userName,
+				lastUpdated:new Date()
+			  }
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST",urlPrefix+"/query",true);
+	xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+	xhr.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+				 //alert(this.responseText);
+				 console.log("query added");
+				 
+					
+		}
+	};
+		
+	xhr.send(JSON.stringify(query));
 }
 
