859612023ad03c2dcd27b82041f9dbcd34ad1464
Hiresh Trivedi <hiresh.at@gmail.com>
2018-10-02 08:24:28 +0530

    add query via context menu

--- saveMySQL/background.js
+++ saveMySQL/background.js
@@ -6,6 +6,7 @@ chrome.contextMenus.create ({
     onclick: saveQueryToServer
 })
 var userName="";
+var count=0;
 
 chrome.storage.local.get(['userName'],function(result){
 	if(result.userName && result.userName!=''){
@@ -16,13 +17,34 @@ chrome.storage.local.get(['userName'],function(result){
 });
 
 function saveQueryToServer(text) {
+	chrome.storage.local.get(['userName'],function(result){
+		if(result.userName && result.userName!=''){
+			userName=result.userName;
+		}
+	});
+	//TODO: set a counter in local storage and append it to QueryName everytime and then increase it
+	chrome.storage.local.get(['contextMenuCounter'],function(result){
+		if(result){
+			count=result.contextMenuCounter;
+			count++;
+			chrome.storage.local.set({"contextMenuCounter":count},function(){
+				saveQueryViaContextMenu(text);
+			});
+		}
+	});
+	
+	
+}
+
+function saveQueryViaContextMenu(text){
 	var query={
 				queryText:text.selectionText,
-				queryName:"Query",
+				queryName:"Query "+count,
 				description:"added from contextMenu",
 				author:userName,
 				lastUpdated:new Date()
 			  }
+	
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST",urlPrefix+"/query",true);
 	xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8");
