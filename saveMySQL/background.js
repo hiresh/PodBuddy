34b6eeba82de3e786bf15191c59b7b4f55ddc990
Hiresh Trivedi <hiresh.at@gmail.com>
2019-05-12 14:41:10 +0530

    Notifications on success/failure of adding query

--- saveMySQL/background.js
+++ saveMySQL/background.js
@@ -69,8 +69,9 @@ function saveQueryToServer(text) {
 	
 }
 
+//convert this in jquery ajax call and handle errors
 function saveQueryViaContextMenu(queryName, queryDescription, text,userName) {
-
+debugger;
 	var query = {
 		"queryText": text.selectionText.replace(/"/g, "'"),
 		"queryName": queryName,
@@ -83,14 +84,54 @@ function saveQueryViaContextMenu(queryName, queryDescription, text,userName) {
 	xhr.open("POST", urlPrefix + "/query", true);
 	xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8");
 	xhr.onreadystatechange = function () {
+		//debugger;
+		//console.log('bg js');
 		if (this.readyState == 4 && this.status == 200) {
 			console.log("query added");
+			
+			notifObject={
+							type:'basic',
+							iconUrl:'dbicon.png',
+							title:'Query saved',
+							message:queryName+' published'
+						}
+			//chrome.notifications.clear(id, function ncb(){})
+			sendDesktopNotif(queryName+''+Math.random(),notifObject,function(id){})
 		}
-		if (this.readyState == 4 && this.status == 500) {
-			var errorResponse = JSON.parse(this.responseText)
-			alert(errorResponse.message)
+		else if (this.readyState == 4 ) {
+			
+			var errorResponse =null;
+			if(this.responseText)
+			errorResponse= JSON.parse(this.responseText)
+			//alert(errorResponse.message)
+			console.log('entered errored state');
+			errObject={
+					type:'basic',
+							iconUrl:'errorIcon.png',
+							title:'Unable to save Query',
+							message:'Please check if you are connected to VPN'
+			}
+			sendDesktopNotif(queryName+''+Math.random(),errObject,function(id){});
 		}
 	};
 	xhr.send(JSON.stringify(query));
 }
 
+function sendDesktopNotif(notifName,notifObject,notifCallBack){
+	/*
+	*
+	*{
+        type: 'basic',
+        iconUrl: 'icon.png',
+        title: 'Don\'t forget!',
+        message: 'You have  things to do. Wake up, dude!'
+	 }
+	* callback has notifId as argument
+	*
+	*
+	*/
+	console.log('sending notif');
+	chrome.notifications.create(notifName, notifObject, notifCallBack);
+	
+
+}
